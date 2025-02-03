82729c9a43a560be08ca884bbffceb9c309dfc36
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-02-03 16:26:58 +0530

    Update construct-binary-search-tree-from-preorder-traversal.cpp

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,6 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
+T.C-O(3N);
+The solution uses an upper bound and an external pointer to build a BST out 
+The index pointer traverses the preorder vector array .
+These parameters are passed through the build function
+In this tree gets constructed
+1)Base case:
+     This is when index reaches the end of the preorder array or the value of the array exceeds the bound. This should return null and go back to the previous condition
+
+We then construct a new node that stores the iterating values of the vector.
+
+2)Normal case:
+     left of this node is passed recursively through the build function with the bound set at value of the root to satisfy basic BST conditions
+     right of this node is passed recursively through the build function with the bound set at the earlier prexisting bound(INT_MAX initially and then the suceeding root values)
+     
+    */
 class Solution {
 private:
     TreeNode*build(vector<int>&v,int &i,int bound){
