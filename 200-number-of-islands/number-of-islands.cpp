a0724b06fdafbe8f7f69f6a56f922a8d31971249
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-02-12 00:37:14 +0530

    Time: 20 ms (93.96%) | Memory: 23.1 MB (35.78%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -29,6 +29,7 @@ private:
 
 public:
     int numIslands(vector<vector<char>>& grid) {
+        ios_base::sync_with_stdio(false);
         int count = 0;
         int n = grid.size();
         int m = grid[0].size();
