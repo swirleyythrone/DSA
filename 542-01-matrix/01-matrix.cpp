7c93c6976d8e1d4854562f9af7cc54969137cbd9
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-02-15 11:30:50 +0530

    Time: 17 ms (66.42%) | Memory: 34.5 MB (93.32%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+       vector<int> dir={0,1,0,-1,0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m = mat[0].size();
+        queue<pair<int,int>>    q;
+        for(int i=0 ; i < n ;  i++){
+            for(int j=0 ; j< m ; j++){
+                if(mat[i][j]==0)q.emplace(i,j);
+                else mat[i][j]=-1;
+            }
+        }
+        while(!q.empty()){
+            auto[r,c]=q.front();
+            q.pop();
+            for(int i = 0 ; i < 4 ; ++i){
+                int nr=r+dir[i];
+                int nc = c+dir[i+1];
+                if(nr<0 || nr==n || nc<0 || nc==m || mat[nr][nc]!=-1)continue;
+                mat[nr][nc]=mat[r][c]+1;
+                q.emplace(nr,nc);
+            }
+        }
+        return mat;
+    }
+};
