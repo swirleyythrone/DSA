407e2c735892574a72025abfa1603394803acaa6
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-04-19 23:29:42 +0530

    Time: 14 ms (34.46%) | Memory: 17.3 MB (67.97%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,8 +12,8 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
      ios_base::sync_with_stdio(false);
         if(root==NULL || root==p || root==q) return root;
-        TreeNode*right=lowestCommonAncestor(root->right,p,q);
-        TreeNode*left=lowestCommonAncestor(root->left,p,q);
+        TreeNode*right=lowestCommonAncestor(root->left,p,q);
+        TreeNode*left=lowestCommonAncestor(root->right,p,q);
         if(left==NULL)return right;
         else if(right==NULL)return left;
         else return root;
