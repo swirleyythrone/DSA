5630fa06b39b9d3740d5e8fd0700039309775505
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-04-19 12:24:25 +0530

    Time: 5 ms (12.70%) | Memory: 19.5 MB (8.43%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+
+public:
+    string largestOddNumber(string num) {
+        ios_base::sync_with_stdio(false);
+        int n = num.length();
+        int flag=-1;
+        for(int i=n-1; i>=0 ; i-- ){
+            if((num[i]-'0')%2==1){
+                flag=i;
+                break;
+            }
+        }
+        string result="";
+        for(int j = 0 ; j<=flag ; j++){
+            result+=num[j];
+        }
+        if(flag==-1)return "";
+        return result;
+    }
+};
