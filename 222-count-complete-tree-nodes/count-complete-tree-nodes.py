e6c06423fe4883ba256f3904f3b7d8d1347c3aee
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-04-20 23:12:12 +0530

    Time: 3 ms (57.55%) | Memory: 23.4 MB (38.52%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Solution:
+    def countNodes(self, root):
+        if not root:
+            return 0
+        queue = deque([root])
+        count = 0
+        
+        while queue:
+            node = queue.popleft()
+            count += 1
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return count
