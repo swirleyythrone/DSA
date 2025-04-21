017b87c53c7500267b47c37ad2f1c30d66b9dc28
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-04-21 21:39:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.48%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
         int n=nums.size();
-        int lo=0,hi=n-1;
-        
-        while(lo<=hi){
-        int mid=lo+(hi-lo)/2;
+        int s=0,e=n-1;
+        int mid;
+        while(s<=e){
+           mid=s+(e-s)/2;
            if(nums[mid]==target)return mid;
-           else if(nums[mid]<target){
-              lo=mid+1;
-           }
-           else{
-             hi=mid-1;
-           }
+           else if(nums[mid]>target)e=mid-1;
+           else s=mid+1;
         }
         return -1;
-
     }
 };
