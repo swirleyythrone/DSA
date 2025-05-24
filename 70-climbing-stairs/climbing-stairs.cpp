ed9619b9afacd43c022c4f7ec9d19affac86b74d
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-05-24 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (53.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int f(int n,vector<int> &dp){
-        if(n<1)return 0;
-        if(n<3)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    using ll = long long;
+
+    void multiply(vector<vector<ll>>& a, vector<vector<ll>>& b) {
+        vector<vector<ll>> res(2, vector<ll>(2));
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 2; ++j)
+                for (int k = 0; k < 2; ++k)
+                    res[i][j] += a[i][k] * b[k][j];
+        a = res;
     }
+
+    void matrixPower(vector<vector<ll>>& mat, int n) {
+        vector<vector<ll>> res = {{1, 0}, {0, 1}}; // identity matrix
+        while (n > 0) {
+            if (n % 2 == 1)
+                multiply(res, mat);
+            multiply(mat, mat);
+            n /= 2;
+        }
+        mat = res;
+    }
+
     int climbStairs(int n) {
-        ios_base::sync_with_stdio(false);
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
+        if (n == 0 || n == 1) return 1;
+
+        vector<vector<ll>> mat = {{1, 1}, {1, 0}};
+        matrixPower(mat, n);
+        return mat[0][0];
     }
 };
