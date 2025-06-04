1a2e0cac25e45bcab2fbd118b87107e0cf463851
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-06-04 11:30:56 +0530

    Time: 11 ms (98.17%) | Memory: 9.4 MB (88.20%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        ios_base::sync_with_stdio(false);
+        int n = text1.size();
+        int m = text2.size();
+        vector<int> prev(m+1,0),curr(m+1,0);
+
+        for(int j = 0 ; j <= m ; j++)prev[j]=0;
+
+        for(int ind1 = 1 ; ind1 <= n ; ind1++){
+            for(int ind2 = 1 ; ind2 <= m ; ind2++){
+
+                if(text1[ind1-1]==text2[ind2-1]){
+                    curr[ind2] = 1+prev[ind2-1];
+                }
+
+                else{
+                 curr[ind2] = max(prev[ind2],curr[ind2-1]);
+                }
+
+            }
+            prev=curr;
+        }
+
+        return prev[m];
+    }
+};
