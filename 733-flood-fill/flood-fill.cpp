a544b8d3815aa4df73064a0e14b03728a2924961
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-06-20 12:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (61.70%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,24 +1,25 @@
 class Solution {
-private:
-   void dfs(vector<vector<int>> &ans,vector<vector<int>> &image, int sr,int sc,int in_color, int del_row[],int del_col[],int color){
-     ans[sr][sc]=color;
-     int n = image.size(),m= image[0].size();
-     for(int i = 0 ;i < 4 ; i++){
-        int nrow=sr+del_row[i];
-        int ncol=sc+del_col[i];
-        if(nrow>=0 && nrow <n && ncol>=0 && ncol<m && ans[nrow][ncol]!=color && image[nrow][ncol]==in_color){
-            dfs(ans,image,nrow,ncol,in_color,del_row,del_col,color);
-        }
-     }
-   }
 public:
+    void dfs(vector<vector<int>> &ans,vector<vector<int>>& image,int in_color,int color,int sr,int sc,int dr[],int dc[]){
+        ans[sr][sc]=color;
+        int n = ans.size();
+        int m = ans[0].size();
+        for(int i = 0 ; i < 4 ; i++){
+            int nr = sr + dr[i];
+            int nc = sc + dc[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==in_color && ans[nr][nc]!=color){
+                dfs(ans,image,in_color,color,nr,nc,dr,dc);
+            }
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-         ios_base::sync_with_stdio(false);
-        vector<vector<int>> ans =image;
-        int in_color= image[sr][sc];
-        int del_row[4]={-1,0,1,0};
-        int del_col[4]={0,1,0,-1};
-        dfs(ans,image,sr,sc,in_color,del_row,del_col,color);
+        int n = image.size();
+        int m = image[0].size();
+        int in_color=image[sr][sc];
+        vector<vector<int>> ans=image;
+        int dr[4] = {-1,0,+1,0};
+        int dc[4] = {0,-1,0,+1};
+        dfs(ans,image,in_color,color,sr,sc,dr,dc);
         return ans;
     }
 };
