5067efc82149af672fca0a45a17da1ea2f2de752
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-06-25 22:15:56 +0530

    Time: 7 ms (76.94%) | Memory: 33.2 MB (62.46%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,32 +1,29 @@
 class Solution {
-private:
-    void dfs(int i,int j,vector<vector<int>> &grid){
+public:
+    void dfs(int i,int j, vector<vector<int>> &grid){
         int n = grid.size();
         int m = grid[0].size();
         if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0)return;
         grid[i][j]=0;
-        dfs(i+1,j,grid);
         dfs(i-1,j,grid);
         dfs(i,j+1,grid);
+        dfs(i+1,j,grid);
         dfs(i,j-1,grid);
     }
-public:
     int numEnclaves(vector<vector<int>>& grid) {
-        ios_base::sync_with_stdio(false);
         int n = grid.size();
         int m = grid[0].size();
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(i==0 || j == 0 || n-1 == i || m-1 == j && grid[i][j]==1)dfs(i,j,grid);
+                if(i==0 || i==n-1 || j==0 || j==m-1 && grid[i][j]==1)dfs(i,j,grid);
             }
-            
         }
-        int s= 0; 
-            for(int i = 0; i< n ; i++){
-                for(int j = 0 ; j <m ; j++){
-                    s+=grid[i][j];
-                }
+        int c = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                c+=grid[i][j];
             }
-        return s;
+        }
+        return c;
     }
 };
