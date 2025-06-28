a0bf6d6fa552e320fc26758655978b82549fa2bb
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-06-28 17:58:06 +0530

    Time: 11 ms (12.02%) | Memory: 18.7 MB (36.52%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,37 +1,34 @@
 class Solution {
-private:
-    bool dfs(vector<vector<int>>& pre_list, int node, vector<int>& vis, vector<int>& ans) {
-        if (vis[node] == 1) return true;  // Cycle detected
-        if (vis[node] == 2) return false; // Already processed
-
-        vis[node] = 1;
-        for (int it : pre_list[node]) {
-            if (dfs(pre_list, it, vis, ans)) return true;
-        }
-
-        vis[node] = 2;
-        ans.push_back(node);
-        return false;
-    }
-
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        ios_base::sync_with_stdio(false);
-        
-        vector<vector<int>> pre_list(numCourses);
-        for (const auto& it : prerequisites) {
-            pre_list[it[0]].push_back(it[1]);
+        int V = numCourses;
+        vector<vector<int>> adj(V);
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
         }
-
-        vector<int> vis(numCourses, 0);
-        vector<int> ans;
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(pre_list, i, vis, ans)) return {}; // Cycle detected
+        vector<int> indegree(V,0);
+        for(int i = 0 ; i < V ; i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
+        queue<int> q;
+        vector<int> topo;
+        for(int i = 0 ; i < V ; i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it); 
+            }
+        }
+        if(topo.size()==V)return topo;
+        return {};
 
-        return ans;
     }
 };
