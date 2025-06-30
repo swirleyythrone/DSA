38b99e0c95a0436c3c779c5b0c63d8e8ec55f453
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-06-30 11:58:05 +0530

    Time: 48 ms (73.18%) | Memory: 24.6 MB (55.90%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        ios_base::sync_with_stdio(false);
-        //{diff,{row,col}}
-        priority_queue<pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>> >,greater<pair<int,pair<int,int>>>>pq;
         int n = heights.size();
         int m = heights[0].size();
-        vector<vector<int>> d(n,vector<int>(m,1e9));
-        d[0][0]=0;
-        pq.push({0,{0,0}});
-        int drow[4]={-1,0,1,0};
-        int dcol[4]={0,1,0,-1};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+         pq.push({0,{0,0}});
+        dist[0][0] = 0;
+ 
+        int dr[4] = {-1,0,+1,0};
+        int dc[4] = {0,+1,0,-1};
+       
         while(!pq.empty()){
             auto it = pq.top();
-            pq.pop();
             int diff = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
-            if(row==n-1 && col == m-1)return diff;
+            int r = it.second.first;
+            int c = it.second.second;
+            pq.pop();
+
+            if(r==n-1 && c==m-1)return diff;
             for(int i = 0 ; i < 4 ; i++){
-                int nrow = row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m ){
-                    int new_effort=max(abs(heights[nrow][ncol]-heights[row][col]),diff);
-                    if(new_effort<d[nrow][ncol]){
-                        d[nrow][ncol]=new_effort;
-                        pq.push({new_effort,{nrow,ncol}});
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m){
+                    int newEffort = max(abs(heights[nr][nc]-heights[r][c]),diff);
+                    if(newEffort < dist[nr][nc]){
+                        dist[nr][nc] = newEffort;
+                        pq.push({newEffort,{nr,nc}});
                     }
                 }
+                
             }
         }
-        return 0;
-
+        return -1;
     }
 };
