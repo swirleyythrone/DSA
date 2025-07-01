0450924d390d4cbaaa87557efe928328dd634eba
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-01 14:33:40 +0530

    Time: 11 ms (83.73%) | Memory: 17 MB (64.40%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        ios_base::sync_with_stdio(false);
-
         vector<vector<int>> dist(n,vector<int>(n,1e9));
-        for(auto it:edges){
-            dist[it[0]][it[1]]=it[2];
-            dist[it[1]][it[0]]=it[2];
+        for(auto it: edges){
+             dist[it[0]][it[1]]=it[2];
+             dist[it[1]][it[0]]=it[2];
         }
-        for(int i = 0 ;  i < n ; i++)dist[i][i]=0;
-        for(int k = 0 ; k  < n ; k++){
-            for(int i =0 ; i< n ; i++){
-                for(int j = 0; j< n ; j++){
-                    if(dist[i][k]==1e9 || dist[j][k]==1e9)continue;
-                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[j][k]);
+        for(int i = 0 ; i < n ; i++)dist[i][i]=0;
+        for(int via = 0 ; via < n ; via++){
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < n ; j++){
+                    if(dist[i][via]==1e9 || dist[via][j]==1e9)continue;
+                    dist[i][j] = min(dist[i][j],dist[i][via] + dist[via][j]);
                 }
             }
         }
-        int cntCity=n;
-        int city_no=-1;
-        for(int i = 0;i< n ;i++){
-            int cnt=0;
+        int cnt_City =n,city_no=-1;
+        for(int i = 0 ; i < n ; i++){
+            int cnt = 0;
             for(int j = 0 ; j < n ; j++){
                 if(dist[i][j]<=distanceThreshold){
                     cnt++;
                 }
             }
-            if(cnt<=cntCity){
-                cntCity=cnt;
-                city_no=i;
+            if(cnt<=cnt_City){
+                cnt_City=cnt;
+                city_no = i;
             }
         }
         return city_no;
+
+
     }
 };
