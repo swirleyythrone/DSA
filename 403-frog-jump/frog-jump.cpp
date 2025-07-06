b639513dcfeadbe57fc9536688d810ae65272b93
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-06 12:11:01 +0530

    Time: 163 ms (22.91%) | Memory: 28 MB (80.70%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
 map<int,int>mpp;
-
-    int f(int i,int k,vector<int> &stones,vector<vector<int>> &dp,int n){
-        if(i ==n-1)return true;
-        if(dp[i][k]!=-1)return dp[i][k];
-        bool k0=false,k1=false,k2=false;
-        if(k>1 && mpp.find(stones[i] +k-1)!=mpp.end()){
-            k0 = f(mpp[stones[i]+k-1],k-1,stones,dp,n);
-        }
-        if(mpp.find(stones[i]+k)!=mpp.end()){
-            k1 = f(mpp[stones[i]+k],k,stones,dp,n);
-        }
-        if(mpp.find(stones[i]+k+1)!=mpp.end()){
-            k2 = f(mpp[stones[i]+k+1],k+1,stones,dp,n);
-        }
-        return dp[i][k] = k0||k1||k2;
-    }
     bool canCross(vector<int>& stones) {
+        
         int n = stones.size();
-        vector<vector<int>> dp(n,vector<int>(2001,-1));
+        vector<vector<bool>> dp(n,vector<bool>(n+1,false));
 
         if(stones[1]-stones[0]!=1)return false;
+        dp[1][1]=true;
+
         for(int i = 0 ; i < n ; i++){
             mpp[stones[i]]=i;
         }
+
+        for(int i = 1 ; i < n ; i++){
+            for(int k = 1 ; k <= n ; k++){
+                if(dp[i][k]){
+                    for(int jump = k-1 ; jump <= k+1 ; jump++){
+                        if(jump>0 && mpp.find(stones[i]+jump)!=mpp.end()){
+                            int nextInd = mpp[stones[i]+jump];
+                            dp[nextInd][jump] = true;
+                        }
+                    }
+                }
+            }
+        }
+        for(int k = 1 ; k <= n ; k++){
+            if(dp[n-1][k])return true;
+        }
         
-        return f(1,1,stones,dp,n);
+        return false;
     }
 };
