5259febd2673a6d94dda815732a48b3010aecd7f
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-09 17:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 59.4 MB (25.86%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -3,18 +3,18 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         ios_base::sync_with_stdio(false);
         int n = nums.size();
-        int l = 0, r = 0, zeros = 0, maxlen = 0;
+        int l=0,r=0,maxLen=0,zeros=0;
         while(r<n){
             if(nums[r]==0)zeros++;
             if(zeros>k){
                 if(nums[l]==0)zeros--;
                 l++;
-            }else{
-                int len = r-l+1;
-                maxlen = max(len,maxlen);
+            }
+            else{
+                maxLen = max(r-l+1,maxLen);
             }
             r++;
         }
-        return maxlen; 
+        return maxLen;
     }
 };
