a26e31f495190669449fe2260fe325149e035a32
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-12 10:25:00 +0530

    Time: 50 ms (17.66%) | Memory: 105.8 MB (60.40%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,17 @@
+// OJ: https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+// Author: github.com/lzl124631x
+// Time: O(NlogN)
+// Space: O(1)
+class Solution {
+public:
+    int maxFrequency(vector<int>& A, int k) {
+        sort(begin(A), end(A));
+        long i = 0, N = A.size(), ans = 1, sum = 0;
+        for (int j = 0; j < N; ++j) {
+            sum += A[j];
+            while ((j - i + 1) * A[j] - sum > k) sum -= A[i++];
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
