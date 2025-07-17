44c091796c0b78bdbeff4aaf649162bee88fa9b9
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-17 11:56:47 +0530

    Time: 55 ms (51.57%) | Memory: 61.8 MB (43.21%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return false;
+
+            }
+        }
+        return true;
+    }
+    void f(int index,string s,vector<string> &path,vector<vector<string>> &res){
+        if(index==s.size()){
+            res.push_back(path);
+            return;
+        }
+        for(int i = index ; i < s.size() ; i++){
+            if(isPalindrome(s,index,i)){
+                path.push_back(s.substr(index,i-index+1));
+                f(i+1,s,path,res);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>path;
+        f(0,s,path,ans);
+        return ans;
+    }
+
+};
