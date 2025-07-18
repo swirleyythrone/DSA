ea33e50d02896b468d9f6712fcfe9bed842fd59d
Aryan Nawathe <132645349+swirleyythrone@users.noreply.github.com>
2025-07-18 11:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (48.05%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,9 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        ios_base::sync_with_stdio(false);
         if(p==NULL && q==NULL)return true;
         if(p==NULL || q==NULL)return false;
-        return(p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+        return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
